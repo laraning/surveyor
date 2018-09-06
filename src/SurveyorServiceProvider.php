155531c148317c7da50881e5959abfa5e17f5f72
Laraning <bruno.falcao@laraning.com>
2018-09-06 22:36:09 +0200

    first commit

---
+++ src/SurveyorServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Laraning\Surveyor;
+
+use Illuminate\Support\ServiceProvider;
+
+class SurveyorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
