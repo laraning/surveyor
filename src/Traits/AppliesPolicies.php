dc34e6e632a0950fa75a5e079ea55510bd4b2a44
Laraning <bruno.falcao@laraning.com>
2018-09-16 00:55:03 +0200

    wip

--- src/Traits/AppliesPolicies.php
+++ src/Traits/AppliesPolicies.php
@@ -18,36 +18,36 @@ trait AppliesPolicies
 
     public function viewAny()
     {
-        return $this->repository['policy'][get_called_class()][debug_backtrace()[0]['function']];
+        return $this->repository['policy'][get_called_class()]['viewAny'];
     }
 
     public function view()
     {
-        return $this->repository['policy'][get_called_class()][debug_backtrace()[0]['function']];
+        return $this->repository['policy'][get_called_class()]['view'];
     }
 
     public function create()
     {
-        return $this->repository['policy'][get_called_class()][debug_backtrace()[0]['function']];
+        return $this->repository['policy'][get_called_class()]['create'];
     }
 
     public function update()
     {
-        return $this->repository['policy'][get_called_class()][debug_backtrace()[0]['function']];
+        return $this->repository['policy'][get_called_class()]['update'];
     }
 
     public function delete()
     {
-        return $this->repository['policy'][get_called_class()][debug_backtrace()[0]['function']];
+        return $this->repository['policy'][get_called_class()]['delete'];
     }
 
     public function restore()
     {
-        return $this->repository['policy'][get_called_class()][debug_backtrace()[0]['function']];
+        return $this->repository['policy'][get_called_class()]['restore'];
     }
 
     public function forceDelete()
     {
-        return $this->repository['policy'][get_called_class()][debug_backtrace()[0]['function']];
+        return $this->repository['policy'][get_called_class()]['forceDelete'];
     }
 }
